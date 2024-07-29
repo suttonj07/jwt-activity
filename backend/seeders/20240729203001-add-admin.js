fca1a9cd7983e245fc629ebe3d42bad5bb0529cc
suttonj07 <suttonj07@gmail.com>
2024-07-29 16:36:10 -0400

    jwt complete 2

---
+++ backend/seeders/20240729203001-add-admin.js
@@ -0,0 +1,22 @@
+'use strict';
+const bcrypt = require('bcrypt')
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('users', [{
+      first_name: 'John',
+      last_name: 'Sutton',
+      email: 'john@example.com',
+      role: 'admin',
+      password_digest: await bcrypt.hash(process.env.ADMIN_PASSWORD, 10),
+      created_at: new Date(),
+      updated_at: new Date()
+    }])
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('users', {
+      email: 'john@example.com'
+    })
+  }
+}
